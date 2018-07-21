0cb3db4f47bfa80d157ed80c7b54e6163b82c7cd
hsk <sakurai777@gmail.com>
2018-07-21 19:05:43 +0900

    added bulletpl1

---
+++ bulletpl/mini/bulletpl1.pl
@@ -0,0 +1,72 @@
+:- expects_dialect(sicstus).
+M.del(K) := M2 :- del_dict(K,M,_,M2);M=M2.
+%:- R=a{}.del(a),writeln(R). :- halt.
+dir(_,dirAbs(D),_,D).
+dir(B,dirSeq(D),_,D_) :- D_ is B.pdir+D.
+dir(B,dirAim(D),Ship,D_) :- Ship.y = B.y, Ship.x > B.x, D_ is  3.141592 / 2.0 + D.
+dir(B,dirAim(D),Ship,D_) :- Ship.y = B.y,               D_ is -3.141592 / 2.0 + D.
+dir(B,dirAim(D),Ship,D_) :- D_ is atan(Ship.x - B.x / Ship.y - B.y) + D.
+spd(_,spdAbs(S),_,S).
+spd(B,spdSeq(S),_,S_) :- S_ is B.pdir+S.
+
+chgDir(B,Ship,D,B1) :- (_,_,M,M)=B.get(chgDir),
+  dir(B,B.dir,Ship,D),B1=B.del(chgDir).
+chgDir(B,Ship,D,B1) :- (OD,ND,C,M)=B.get(chgDir),!,
+  dir(B,OD,Ship,OD1),dir(B,ND,Ship,ND1),
+  C1 is C + 1,D is OD1*(1-C1/M)+ND1*C1/M,
+  B1 = B.put(chgDir,(OD,ND,C1,M)).
+chgDir(B,Ship,D,B) :- dir(B,B.dir,Ship,D).
+chgSpd(B,Ship,S,B1) :- (_,_,M,M)=B.get(chgSpd),!,
+  spd(B,B.spd,Ship,S),B1=B.del(chgSpd).
+chgSpd(B,Ship,S,B1) :- (OS,NS,C,M)=B.get(chgSpd),!,
+  spd(B,OS,Ship,OS1),spd(B,NS,Ship,NS1),
+  C1 is C + 1,S is OS1*(1-C1/M)+NS1*C1/M,
+  B1 = B.put(chgSpd,(OS,NS,C1,M)).
+chgSpd(B,Ship,S,B) :- spd(B,B.spd,Ship,S).
+
+moveDefault :-
+  bb_get(ship,Ship),
+  bb_get(bullet,B),
+  chgDir(B,Ship,D,B1),
+  chgSpd(B1,Ship,S,B2),
+  Y is B.y + cos(D)*S,
+  X is B.x - sin(D)*S,
+  NB=B2.put(x,X).put(y,Y).put(pdir,D).put(pspd,S),
+  bb_put(bullet,NB).
+
+moveBullet(A) :- writeln(moveBullet(A)),fail.
+moveBullet(action(As)) :- maplist(moveBullet,As).
+moveBullet(wait(0)) :- !.
+moveBullet(wait(N)) :- moveDefault,shift(N), N1 is N - 1, moveBullet(wait(N1)).
+moveBullet(fire(bullet(D,S,As))) :-
+  bb_get(bullet,B),
+  bb_update(bullets,Bs,[bullet{x:B.x,y:B.y,dir:D,spd:S,cont:moveBullet(action(As))}|Bs]).
+moveBullet(changeDirection(D,T)) :- bb_update(bullet,B,B.put(chgDir,(B.dir,D,0,T))).
+moveBullet(changeSpeed(S,T)) :- bb_update(bullet,B,B.put(chgSpd,(B.spd,S,0,T))).
+moveBullet(repeat(0,_)) :- !.
+moveBullet(repeat(N,As)) :- moveBullet(action(As)),N1 is N - 1,moveBullet(repeat(N1,As)).
+runBullet(B) :-
+  bb_put(bullet,B),
+  reset(B.cont,_,Cont),
+  (var(Cont),!;Cont=0,writeln(0)
+  ;bb_get(bullet,B1),bb_update(bullets,Bs,[B1.put(cont,Cont)|Bs])).
+
+display:-bb_get(bullets,Bs),maplist(drawBullet,Bs).
+
+drawBullet(B) :- writeln(B.del(cont).del(chgSpd).del(chgDir)).
+
+runBullets :-
+  bb_update(bullets,Bs,[]),!,
+  (Bs=[] -> true
+  ;maplist(runBullet,Bs),!,
+   display,
+   runBullets).
+
+:-
+  bb_put(ship,ship{x:400,y:400}),!,
+  bb_put(bullets,[bullet{x:200,y:200,dir:dirAim(0),spd:spdAbs(0),cont:moveBullet(action([
+    fire(bullet(dirAim(0),spdAbs(1),[changeSpeed(spdAbs(2),100),wait(100)])),
+    wait(3)
+  ]))}]),!,
+  runBullets.
+:- halt.
--- main.pl
+++ main.pl
@@ -7,174 +7,6 @@
 :- use_module(library(plGLU)).
 :- use_module(library(plGLUT)).
 :- expects_dialect(sicstus).
-:- bb_put(rank,0).
-rank(R) :- bb_get(rank,R).
-:- bb_put(enemies,[]).
-enemies(Es) :- bb_get(enemies,Es).
-
-/*
-private function onEnter(e:Event):void {
-  for each (var enemy:Enemy in enemys) {
-          enemy.move();
-  }
-  if(enemys.length == 0 || Math.random() < 0.01 * rank) {
-      switch(int(Math.random() * 3)){
-      case 0: addEnemy(0,Math.random()*200,0); break;
-      case 1: addEnemy(Math.random()*465,0,Math.PI/2); break;
-      case 2: addEnemy(465,Math.random()*200,Math.PI); break;
-      }
-  }
-  ship.move();
-  for each (var b:Bullet in bullets) {
-          b.move();
-          if(Math.abs(ship.x-b.x)<3 && Math.abs(ship.y-b.y)<3) {
-              addParticles(ship.x, ship.y, 100);
-              ship.exists = false;
-              ship.visible = false;
-              gameover.visible = true;
-          }
-      }
-  for each (var s:Shot in shots) {
-          for each(enemy in enemys) {
-              if(enemy.visible && Math.abs(enemy.x-s.x)<30 && Math.abs(enemy.y-s.y)<30) {
-                  score += 100;
-                  rank = score / 1000;
-                  addParticles(enemy.x, enemy.y,30);
-                  enemy.visible = false;
-              }
-          }
-          s.move();
-      }
-  for each (var p:Particle in particles)
-          p.move();
-}
-public function addParticles(x:Number, y:Number, n:int):void {
-  for (var i:int = 0; i < n; i++)
-      addParticle(x,y,Math.random() * 100);
-}
-
-public function addBulletN(x:Number, y:Number, rad:Number,n:int, r:Number):void {
-  for (var i:int = 0; i < n; i++) {
-    addBullet(x,y,rad+(i-n/2.0)*r);
-  }
-}
-
-newEnemy(Env,X,Y,E) :-
-  Rad is atan((Y-Env.my)/(X-Env.mx)),
-  E=enemy{x:X,y:Y,rad:Rad,move:enemy_move1}.
-
-enemy_move1(Env,E,E_) :-
-  moveBody(Env,E,R),
-    if(Math.random() > 0.8) {
-        a.addBullet(x,y,r);
-        if(Math.random() > 0.8) {
-            r2=0;
-            wait(30,move2);
-        }
-    }
-}
-
-class Enemy extends Sprite{
-    private var rad:Number = 0;
-    private var r:Number = 0.05;
-    public var move:Function;
-    private var nextMove:Function;
-    private var time:int;
-*/
-
-enemy_moveBody(Env,E,E_,Rad2) :-
-  Rad2 is atan2(Env.my-E.y, Env.mx-E.x),
-  Rad3 is E.rad-Rad2,
-  normalRad(Rad3,NRad),
-  (NRad < 0.0 -> Rad3 is Rad + 0.05
-                ; Rad3 is Rad - 0.05),
-  normalRad(Rad3,Rad4),
-  X1 is X+cos(Rad4)*5.0,
-  Y1 is Y+sin(Rad4)*5.0,
-  clipX(X1,X2),clipY(Y1,Y2),
-  E_=E.put(x,X2).put(y,Y2).put(rad,Rad4).
-enemy_wait(Env,W,E,Next,E2) :-
-  E2 = E.put(time,W).put(move,enemy_moveWait).put(nextMove,Next).
-enemy_moveWait(Env,E,E3) :-
-  enemy_moveBody(Env,E,E1,_),
-  Time is E1.time - 1,
-  E2=E1.put(time,Time),
-  (Time =< 0 -> E3=E2.put(move,E2.nextMove)
-  ; E3=E2).
-
-enemy_move1(Env,E,E2) :-
-  enemy_moveBody(Env,E,E_,R),
-  random(0.0,1.0,R1),
-  (R1 > 0.8 ->
-    % addBullet(x,y,r),
-    random(0.0,1.0,R2),
-    (R2 > 0.8 ->
-      enemy_wait(Env,30,E.put(r2,0.0),enemy_move2,E2)
-    ;E2=E)
-  ;E2=E).
-enemy_move2(Env,E,E2):-
-  enemy_moveBody(Env,E,E_,R),
-  random(0.1,0.6,RR),
-  %addBulletN(x,y,r+r2,rr,0.1),
-  R2 is E_.r2+RR*0.1,
-  E1 = E_.put(r2,R2),
-  random(0.0,1.0,R3),
-  (R3 < 0.03 ->
-    enemy_wait(Env,60,E1,enemy_move1,E2)
-  ;E2=E1).
-
-normalRad(V,R) :- V >  3.141592, R is V - 2.0 * 3.141592.
-normalRad(V,R) :- V < -3.141592, R is V + 2.0 * 3.141592.
-normalRad(V,V).
-
-/*
-class Particle extends Sprite{
-    private var speed:Number = 5;
-    private var rad:Number;
-    public function Particle() {
-        graphics.beginFill(0xffff8800);
-        graphics.drawRect(-2,-2,4,4);
-        graphics.endFill();
-        visible = false;
-    }
-    public function init(x:Number, y:Number, rad:Number):void {
-        this.x = x;
-        this.y = y;
-        this.rad = rad;
-        visible = true;
-        speed = Math.random() * 50 + 10;
-    }
-    public function move():Boolean {
-        x += Math.cos(rad)*speed;
-        y += Math.sin(rad)*speed;
-        if(x < 0 || 465 < x || y < 0 || 465 < y) visible = false;
-        return visible ;
-    }
-}
-
-class Bullet extends Sprite{
-    private var speed:Number = 10;
-    private var rad:Number;
-    public function Bullet() {
-        graphics.beginFill(0xffff8800);
-        graphics.drawRect(-5,-5,10,10);
-        graphics.endFill();
-        visible = false;
-    }
-    public function init(x:Number, y:Number, rad:Number):void {
-        this.x = x;
-        this.y = y;
-        this.rad = rad;
-        visible = true;
-    }
-    public function move():Boolean {
-        x += Math.cos(rad)*speed;
-        y += Math.sin(rad)*speed;
-        if(x < 0 || 465 < x || y < 0 || 465 < y) visible = false;
-        return visible ;
-    }
-}
-*/
 
 {}(A,A).
 <<(B,A,D) :- maplist(call,A,V),append(V,[D],V2),apply(B,V2).
@@ -182,102 +14,282 @@ B << A :- maplist(call,A,V),apply(B,V).
 vertex2f(X,Y) :- Xv is X, Yv is Y, glVertex2f(Xv,Yv).
 \/(A,B,C) :- C is A\/B.
 
-:- 
-  Mh is sqrt(3.0) * 30.0,
-  bb_put(env,env{
-    width:465,height:465,
-    mx:232.5,my:400.0,
-    mh:Mh,
-    shots:[]
-  }).
-
-:- bb_put(key,key{left:0.0,right:0.0,shot:0,trigger:0}).
-
 keyboard(27,_,_) :- glutDestroyWindow. % esc
-keyboard(32,_,_) :- !,bb_update(key,K,K.put(shot,1).put(trigger,1)). % space
-keyboard(R,A,B) :- writeln(R).
-keyboardUp(32,_,_) :- bb_update(key,K,K.put(shot,0)). %space
-keyboardUp(R,A,B) :- writeln(R).
+keyboard(122,_,_) :- !,bb_update(key,K,K.put(shot,1).put(trigger,1)). % space
+keyboard(R,_,_) :- writeln(R).
+keyboardUp(122,_,_) :- bb_update(key,K,K.put(shot,0)). %space
+keyboardUp(R,_,_) :- writeln(R).
 
 special(100,_,_) :- bb_update(key,K,K.put(left,-1.0)).
+special(101,_,_) :- bb_update(key,K,K.put(up,-1.0)).
 special(102,_,_) :- bb_update(key,K,K.put(right,1.0)).
+special(103,_,_) :- bb_update(key,K,K.put(down,1.0)).
 %special(A,_,_) :- writeln(A).
 specialUp(100,_,_) :- bb_update(key,K,K.put(left,0.0)).
+specialUp(101,_,_) :- bb_update(key,K,K.put(up,0.0)).
 specialUp(102,_,_) :- bb_update(key,K,K.put(right,0.0)).
+specialUp(103,_,_) :- bb_update(key,K,K.put(down,0.0)).
 
 timer(_) :-
-  bb_get(env,E),move(E,E_),bb_put(env,E_),
-  glutPostRedisplay,c_glutTimerFunc(10,0).
+  bb_get(env,Env),
+  move,
+  glutPostRedisplay,
+  c_glutTimerFunc(Env.wait,0).
 
-move --> move_ship,move_shots.
-move_ship(Env,Env3) :-
-  bb_update(key,K,K.put(trigger,0)),
-  move_ship_shot(K.shot,Env,Env2),
-  Mx is min(max(Env2.mx + (K.left+K.right)*5,30.0),Env.width-30.0),
-  Env3=Env2.put(mx,Mx).
+:- bb_put(env,env{width:465,height:465,wait:20}).
+:- bb_put(key,key{left:0.0,right:0.0,up:0.0,down:0.0,shot:0,trigger:0}).
+:- bb_put(rank,20).
+:- bb_put(ship,ship{x:232.5,y:400.0}).
+:- bb_put(shots,[]).
+:- bb_put(enemies,[]).
+:- bb_put(bullets,[]).
+:- bb_put(particles,[]).
 
-move_ship_shot(1,Env,Env2) :-
-  random(-0.25,0.25,Rnd),
-  Rad is -3.141592/2.0+Rnd,
-  Env2=Env.put(shots,[shot{x:Env.mx,y:Env.my,rad:Rad}|Env.shots]).
-move_ship_shot(_,E,E).
+clipX(X1,X2) :- X2 is max(0.0,min(465.0,X1)).
+clipY(X1,X2) :- X2 is max(0.0,min(465.0,X1)).
+normalRad(V,R) :- V >  3.141592, R is V - 2.0 * 3.141592.
+normalRad(V,R) :- V < -3.141592, R is V + 2.0 * 3.141592.
+normalRad(V,V).
 
-move_shots(Env,Env.put(shots,Shots)) :- foldl(move_shot(Env),Env.shots,[],Shots).
-move_shot(_,Shot,Shots,[Shot.put(x,Nx).put(y,Ny)|Shots]) :-
-  Shot.y > 0,Shot.y < 465, Shot.x > 0, Shot.x < 465,
-  Nx is Shot.x + cos(Shot.rad) * 25.0,
-  Ny is Shot.y + sin(Shot.rad) * 25.0, !.
-move_shot(_,_,Shots,Shots).
+% ship
 
-/*
-move_ship(Env,Env_.put(mx,Mx)) :-
-  bb_update(key,K,K.put(trigger,0)),
-  %move_ship_shot(K.trigger,Env,Env_),
-  Mx is min(max(Env.mx + (K.left+K.right)*5,30),Env.width-30).
+  moveShip :-
+    bb_update(key,K,K.put(trigger,0)),
+    moveShipShot(K.shot),
+    bb_get(ship,Ship),
+    bb_get(env,Env),
+    X is min(max(Ship.x + (K.left+K.right)*5.0,30.0),Env.width-30.0),
+    Y is min(max(Ship.y + (K.up+K.down)*5.0,30.0),Env.height-30.0),
+    bb_put(ship,Ship.put(x,X).put(y,Y)).
+  moveShipShot(1) :-
+    random(-0.25,0.25,Rnd),
+    Rad is -3.141592/2.0+Rnd,
+    bb_get(ship,Ship),
+    bb_update(shots,Shots,[shot{x:Ship.x,y:Ship.y,rad:Rad}|Shots]).
+  moveShipShot(_).
 
-*/
+  drawShip :- !,
+    bb_get(ship,Ship),
+    glBegin<<[kGL_POLYGON],
+    glColor4f(1.0, 1.0, 0.0, 0.75),
+    vertex2f(Ship.x-10, Ship.y-10),
+    vertex2f(Ship.x+10, Ship.y-10),
+    vertex2f(Ship.x+10, Ship.y+10),
+    vertex2f(Ship.x-10, Ship.y+10),
+    glEnd,!.
 
-draw_ship(Env) :- !,
-	glBegin<<[kGL_POLYGON],
-	glColor4f(1.0, 1.0, 0.0, 0.75),
-  vertex2f(Env.mx-10, Env.my-10),
-  vertex2f(Env.mx+10, Env.my-10),
-  vertex2f(Env.mx+10, Env.my+10),
-  vertex2f(Env.mx-10, Env.my+10),
-	glEnd,!.
+% shot
+  moveShot(It) :-
+    It.y > 0,It.y < 465, It.x > 0, It.x < 465,
+    Nx is It.x + cos(It.rad) * 25.0,
+    Ny is It.y + sin(It.rad) * 25.0, !,
+    bb_update(shots,Shots,[It.put(x,Nx).put(y,Ny)|Shots]),
+    bb_get(enemies,Enemies),
+    exclude(collisionShot(It),Enemies,Enemies_),
+    bb_put(enemies,Enemies_).
+  moveShot(_).
 
-draw_shots(Shots) :- maplist(draw_shot,Shots).
-draw_shot(Shot) :- !,
-	glBegin<<[kGL_POLYGON],
-	glColor4f(0.0, 1.0, 1.0, 0.75),!,
-  vertex2f(Shot.x-10, Shot.y-10),
-  vertex2f(Shot.x+10, Shot.y-10),
-  vertex2f(Shot.x+10, Shot.y+10),
-  vertex2f(Shot.x-10, Shot.y+10),
-	glEnd,!.
+  collisionShot(It,Enemy) :-
+    X is abs(Enemy.x-It.x), X<30.0,
+    Y is abs(Enemy.y-It.y), Y<30.0,
+    %score += 100;
+    %rank = score / 1000;
+    addParticles(Enemy.x, Enemy.y,30).
 
+  moveShots :-
+    bb_update(shots,Shots,[]),!,
+    maplist(moveShot,Shots).
+  drawShots :-
+    bb_get(shots,Shots),
+    maplist(drawShot,Shots).
+  drawShot(Shot) :- !,
+    glBegin<<[kGL_POLYGON],
+    glColor4f(0.0, 1.0, 1.0, 0.75),!,
+    vertex2f(Shot.x-10, Shot.y-10),
+    vertex2f(Shot.x+10, Shot.y-10),
+    vertex2f(Shot.x+10, Shot.y+10),
+    vertex2f(Shot.x-10, Shot.y+10),
+    glEnd,!.
+
+% enemy
+  newEnemy(X,Y) :-
+    bb_get(ship,Ship),
+    (X=Ship.x -> Rad = 0.0; Rad is atan((Y-Ship.y)/(X-Ship.x))),
+    E=enemy{x:X,y:Y,rad:Rad,move:move1Enemy},
+    bb_update(enemies,Enemies,[E|Enemies]).
+  moveEnemy(Enemy) :- M=Enemy.move,call(M,Enemy).
+  moveEnemy(Enemy) :- writeln(error:moveEnemy:Enemy),throw(error).
+  moveBodyEnemy(E,E_,Rad2) :-
+    bb_get(ship,Ship),
+    Rad2 is atan2(Ship.y-E.y, Ship.x-E.x),
+    Rad3 is E.rad-Rad2,
+    normalRad(Rad3,NRad),
+    (NRad < 0.0 -> Rad4 is E.rad + 0.05
+                  ; Rad4 is E.rad - 0.05),
+    normalRad(Rad4,Rad5),
+    X1 is E.x+cos(Rad5)*2.0,
+    Y1 is E.y+sin(Rad5)*2.0,
+    clipX(X1,X2),clipY(Y1,Y2),
+    E_=E.put(x,X2).put(y,Y2).put(rad,Rad5).
+  waitEnemy(W,E,Next) :-
+    E2 = E.put(time,W).put(move,moveWaitEnemy).put(nextMove,Next),
+    bb_update(enemies,Enemies,[E2|Enemies]).
+  moveWaitEnemy(E) :-
+    moveBodyEnemy(E,E1,_),
+    Time is E1.time - 1,
+    E2=E1.put(time,Time),
+    (Time =< 0 -> bb_update(enemies,Enemies,[E2.put(move,E2.nextMove)|Enemies])
+    ; bb_update(enemies,Enemies,[E2|Enemies])).
+  move1Enemy(E) :-
+    moveBodyEnemy(E,E_,R),
+    random(0.0,1.0,R1),
+    (R1 > 0.8 -> !,
+      addBullet(E_.x,E_.y,R),
+      random(0.0,1.0,R2),
+      (R2 > 0.8 -> !,
+        waitEnemy(30,E_.put(r2,0.0),move2Enemy)
+      ;!,bb_update(enemies,Enemies,[E_|Enemies]),!)
+    ;bb_update(enemies,Enemies,[E_|Enemies]),!).
+  move2Enemy(E):-
+    moveBodyEnemy(E,E_,R),
+    random(1,6,RR),
+    R1 is R + E.r2,
+    addBulletN(E_.x,E_.y,R1,RR,0.1),
+    R2 is float(E_.r2+RR*0.1),
+    E1 = E_.put(r2,R2),
+    random(0.0,1.0,R3),
+    (R3 < 0.03 ->
+      waitEnemy(60,E1,move1Enemy)
+    ;bb_update(enemies,Enemies,[E1|Enemies])).
+  addEnemy :-
+    bb_get(rank,Rank),
+    bb_get(enemies,Enemies),length(Enemies,L),
+    (Enemies=[];L<5,random(0.0,100.0,R1),R1<Rank),!,
+    random(0,3,R),addEnemy1(R).
+  addEnemy.
+  addEnemy1(0) :- random(0.0,200.0,Y),newEnemy(100.0,Y).
+  addEnemy1(1) :- random(0.0,456.0,X),newEnemy(X,100.0).
+  addEnemy1(2) :- random(0.0,200.0,Y),newEnemy(100.0,Y).
+  addEnemy1(N) :- writeln(errror_addEnemy1:N).
+  moveEnemies :-
+    bb_update(enemies,Enemies,[]),
+    maplist(moveEnemy,Enemies),
+    %bb_get(enemies,Es),!,writeln(enemies:Es),!,
+    addEnemy,!.
+  drawEnemies :-
+    bb_get(enemies,Enemies),
+    maplist(drawEnemy,Enemies).
+  drawEnemy(Enemy) :- !,
+    glBegin<<[kGL_POLYGON],
+    glColor4f(0.0, 1.0, 1.0, 0.75),!,
+    vertex2f(Enemy.x-10, Enemy.y-10),
+    vertex2f(Enemy.x+10, Enemy.y-10),
+    vertex2f(Enemy.x+10, Enemy.y+10),
+    vertex2f(Enemy.x-10, Enemy.y+10),
+    glEnd,!.
+
+% bullet
+
+  addBulletN(X,Y,Rad,N,R) :-
+    forall(between(1,N,I),(
+      R2 is float(Rad+(I-1-N/2.0)*R),
+      addBullet(X,Y,R2),!
+    )),!.
+  addBullet(X,Y,Rad) :-
+    bb_update(bullets,Bullets,[bullet{x:X,y:Y,rad:Rad,speed:10.0}|Bullets]),!.
+  moveBullet(This) :-
+    X is This.x + cos(This.rad)*This.speed,
+    Y is This.y + sin(This.rad)*This.speed,
+    (X < 0 ; 465 < X ; Y < 0 ; 465 < Y
+    ; bb_update(bullets,Bullets,[This.put(x,X).put(y,Y)|Bullets]),
+      (collisionBullet(This);true)
+    ).
+  collisionBullet(B) :-
+    bb_get(ship,Ship),
+    X is abs(Ship.x-B.x), X<3.0,
+    Y is abs(Ship.y-B.y), Y<3.0,
+    addParticles(Ship.x, Ship.y, 50),
+    %ship.exists = false;
+    %ship.visible = false;
+    %gameover.visible = true;
+    !.
+  drawBullets :-
+    bb_get(bullets,Bullets),
+    glColor4f(1.0, 0.0, 1.0, 0.75),!,
+    maplist(drawBullet,Bullets).
+  drawBullet(It) :- !,
+    glBegin<<[kGL_POLYGON],
+    vertex2f(It.x-4, It.y-4),
+    vertex2f(It.x+4, It.y-4),
+    vertex2f(It.x+4, It.y+4),
+    vertex2f(It.x-4, It.y+4),
+    glEnd,!.
+
+  moveBullets :-
+    bb_update(bullets,Bullets,[]),
+    maplist(moveBullet,Bullets),
+    %bb_get(bullets,Es),!,writeln(bullets:Es),
+    !.
+
+% particle
+    
+  addParticle(X,Y,Rad) :-
+    random(0.0,1.0,Rnd),
+    Speed is float(Rnd * 50+10),
+    Particle = particle{x:X,y:Y,rad:Rad,speed:Speed},
+    bb_get(particles,Particles),length(Particles,L),
+    (L>500 -> true; bb_put(particles,[Particle|Particles])).
+
+  moveParticle(It) :-
+    X is It.x + cos(It.rad)*It.speed,
+    Y is It.y + sin(It.rad)*It.speed,
+    (X < 0 ; 465 < X ; Y < 0 ; 465 < Y
+    ; bb_update(particles,Particles,[It.put(x,X).put(y,Y)|Particles])).
+  moveParticles :-
+    bb_update(particles,Particles,[]),!,
+    maplist(moveParticle,Particles).
+  addParticles(X,Y,N) :-
+    forall(between(1,N,_),(
+      random(0.0,100.0,N1),addParticle(X,Y,N1)
+    )).
+  :- bb_put(maxLen,0).
+  drawParticles :-
+    bb_get(particles,Particles),
+    length(Particles,L),
+    bb_get(maxLen,MaxLen),(L>MaxLen,bb_put(maxLen,L),writeln(L);true),
+    glColor4f(1.0, 0.3, 0.1, 1.0),!,
+    
+    maplist(drawParticle,Particles).
+  drawParticle(It) :- !,
+    glBegin<<[kGL_POLYGON],
+    vertex2f(It.x-4, It.y-4),
+    vertex2f(It.x+4, It.y-4),
+    vertex2f(It.x+4, It.y+4),
+    vertex2f(It.x-4, It.y+4),
+    glEnd,!.
+
+move :- moveShip,moveShots,moveEnemies,moveBullets,moveParticles.
+display :-
+	glClear<<[kGL_COLOR_BUFFER_BIT],
+	drawShip,
+  drawShots,
+  drawEnemies,
+  drawBullets,
+  drawParticles,
+	glFlush.
 reshape :-
   bb_get(env,Env),
 	glViewport(0,0,Env.width,Env.height),
 	glMatrixMode<<[kGL_PROJECTION],
 	glLoadIdentity,
 	gluOrtho2D(0.0, 465.0, 465.0, 0.0).
-
 init:-
 	glEnable<<[kGL_BLEND],
 	glBlendFunc<<[kGL_SRC_ALPHA,kGL_ONE_MINUS_SRC_ALPHA],
 	glShadeModel<<[kGL_FLAT],
 	glClearColor(0.0, 0.0, 0.0, 0.0).
-
-display :- bb_get(env,Env),
-	glClear<<[kGL_COLOR_BUFFER_BIT],
-	draw_ship(Env),
-  draw_shots(Env.shots),
-	glFlush.
-
 idle :- garbage_collect.
-
-:-
+main :-
   bb_get(env,Env),
 	glutInit,
 	glutInitDisplayMode<<[(\/)<<[kGLUT_SINGLE,kGLUT_RGB]],
@@ -291,7 +303,9 @@ idle :- garbage_collect.
 	c_glutKeyboardUpFunc,
 	c_glutSpecialFunc,
 	c_glutSpecialUpFunc,
-  c_glutTimerFunc(10,0),
-%  glutIdleFunc(idle),
+  c_glutTimerFunc(20,0),
+  glutIdleFunc(idle),
 	init,
 	glutMainLoop.
+
+:- main.
